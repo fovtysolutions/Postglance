1361c997c6efabab65bee4ed3f181aa44609e113
shalu patidar <shalupatidar155@gmail.com>
2024-08-30 11:24:16 +0530

    changes

--- inc/core/Settings/Views/settings/menu.php
+++ inc/core/Settings/Views/settings/menu.php
@@ -23,7 +23,7 @@
     </style>
 </head>
 <div class="nav-item custom-hover-effect">
-    <a class="nav-link sp-menu-item actionItem <?php _e( uri('segment', 3) == $config['id']?"active":"" )?>" href="<?php _e( base_url("settings/index/" . $config['id']) )?>" data-remove-other-active="true" data-active="bg-light-primary" data-result="html" data-content="main-wrapper" data-history="<?php _e( base_url("settings/index/" . $config['id']) )?>">
+    <a class="nav-link sp-menu-item actionItem <?php _e( uri('segment', 3) == $config['id']?"active bg-light-primary":"" )?>" href="<?php _e( base_url("settings/index/" . $config['id']) )?>" data-remove-other-active="true" data-active="bg-light-primary"  data-result="html" data-content="main-wrapper" data-history="<?php _e( base_url("settings/index/" . $config['id']) )?>">
       
         <i class="bi bi-house-check-fill dashboard-icon me-4"></i>
        
